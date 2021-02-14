48b31e9dd14db07b68f1972944be69cc701bf4f3
Bruno morikuni <bruno.mkni@hotmail.com>
2021-02-13 21:46:47 -0300

    Delete aula 11 directory

--- aula 11/ex009.js
+++
@@ -1,6 +0,0 @@
-var vel = 70
-console.log(`a velocidade do carro é ${vel}km/h`)
-if (vel > 60 ){ // condição simples!!
-    console.log('Você ultrapassou a velocidade permitida . MULTADO!')
-}
-console.log('Dirija sempre com cinto de seguramça !')
--- aula 11/ex010.js
+++
@@ -1,6 +0,0 @@
-var pais = 'EUA'
-if(pais == 'BRASIL') { // condição composta!
-    console.log('brasileiro!')
-} else {
-    console.log('estranjeiro!')
-}
